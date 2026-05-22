de72eabf9e83ef8d87a4ed54ea0b26fff4261b68
Jesiel Vasconcelos <profjesielvasconcelos@gmail.com>
2026-05-22 11:45:55 -0300

    Atualização de projeto

--- CadastroDeNinjas/src/main/java/dev/java10x/CadastroDeNinjas/CadastroDeNinjasApplication.java
+++ CadastroDeNinjas/src/main/java/dev/java10x/CadastroDeNinjas/CadastroDeNinjasApplication.java
@@ -5,9 +5,10 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class CadastroDeNinjasApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(CadastroDeNinjasApplication.class, args);
+    }
 
-	public static void main(String[] args) {
-		SpringApplication.run(CadastroDeNinjasApplication.class, args);
-	}
+
 
 }
