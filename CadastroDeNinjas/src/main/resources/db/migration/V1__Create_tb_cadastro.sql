c6e6d0d4a145b31cfba56cf230c8812ba88dd4df
Jesiel Vasconcelos <profjesielvasconcelos@gmail.com>
2026-05-27 15:21:19 -0300

    feat: Migrations para tb_cadastro incluindo a column de rank

---
+++ CadastroDeNinjas/src/main/resources/db/migration/V1__Create_tb_cadastro.sql
@@ -0,0 +1,5 @@
+CREATE TABLE tb_cadastro (
+    id BIGINT AUTO_INCREMENT PRIMARY KEY,
+    nome VARCHAR(255),
+    idade INT
+);
---
+++ CadastroDeNinjas/src/main/resources/db/migration/V2__Add_rank_tb_cadastro.sql
@@ -0,0 +1,3 @@
+-- V2: migrations para adicionar a coluna de tank na tabela de cadastro
+
+ALTER TABLE tb_cadastro ADD COLUMN rank VARCHAR(255);
